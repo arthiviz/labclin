f14d01632a1740d2b77f82dc859870e3831a25dc
arthiviz <arthurdasilvasousa7@gmail.com>
2026-03-09 19:10:51 -0300

    variaveis de ambiente

--- labclin/src/main/java/app/config/TokenConfig.java
+++ labclin/src/main/java/app/config/TokenConfig.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
@@ -13,6 +14,7 @@ import java.util.Optional;
 @Component
 public class TokenConfig {
 
+    @Value("${api.security.token.secret}")
     private String secret = "token";
 
     public String generateToken(User user){
